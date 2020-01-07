cc4ada91f82d206efb2323b545a90728b018775e
zxy15914507674 <2847225301@qq.com>
2020-01-07 09:55:59 +0800

    替换wcf服务端

--- 语音合成系统/WCF服务端/Program.cs
+++ 语音合成系统/WCF服务端/Program.cs
@@ -13,25 +13,54 @@ namespace WCF服务端
         static void Main(string[] args)
         {
             //创建一个基地址,MyService名称自己起
-            Uri uri = new Uri("http://47.94.102.147:8000/TTSService");
+            Uri uri1 = new Uri("http://47.94.102.147:8000/TTSService");
+            Uri uri2 = new Uri("http://47.94.102.147:8000/HandService");
+
+
+
             //创建一个Servicehost对象
-            ServiceHost host = new ServiceHost(typeof(TTSService), uri);
-            WSHttpBinding bind = new WSHttpBinding();
-            bind.Security.Mode = SecurityMode.None;
+            ServiceHost host1 = new ServiceHost(typeof(TTSService), uri1);
+            ServiceHost host2 = new ServiceHost(typeof(HandService), uri2);
+
+            WSHttpBinding bind1 = new WSHttpBinding();
+            bind1.Security.Mode = SecurityMode.None;
+
+            WSHttpBinding bind2 = new WSHttpBinding();
+            bind2.Security.Mode = SecurityMode.None;
+
             //为一个服务添加终结点,myHello名称自己起
-            host.AddServiceEndpoint(typeof(ITTSContract), bind, "TTSInterface");
+            host1.AddServiceEndpoint(typeof(ITTSContract), bind1, "TTSInterface");
+            host2.AddServiceEndpoint(typeof(IHandContract), bind2, "HandGestureInterface");
+            
+            
             //建立元数据行为对象
-            ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+            ServiceMetadataBehavior behavior1 = new ServiceMetadataBehavior();
             //设置httpgetEnabled属性
-            behavior.HttpGetEnabled = true;
+            behavior1.HttpGetEnabled = true;
             //把这个服务元数据行为对象添加到宿主ServiceHost对象的行为集合中去
-            host.Description.Behaviors.Add(behavior);
+            host1.Description.Behaviors.Add(behavior1);
+
+
+            //建立元数据行为对象
+            ServiceMetadataBehavior behavior2 = new ServiceMetadataBehavior();
+            //设置httpgetEnabled属性
+            behavior2.HttpGetEnabled = true;
+            //把这个服务元数据行为对象添加到宿主ServiceHost对象的行为集合中去
+            host2.Description.Behaviors.Add(behavior2);
+
+
+
             //启动宿主，开启服务
-            host.Open();
+            host1.Open();
+            host2.Open();
+
+
+
             Console.WriteLine("host is ready");
             Console.ReadLine();
             //关闭服务
-            host.Close();
+            host1.Close();
+            host2.Close();
 
         }
     }
