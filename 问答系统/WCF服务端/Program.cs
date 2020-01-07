68fb869bf2598d4f4f8449dc80a54b6b15f7c983
zxy15914507674 <2847225301@qq.com>
2020-01-07 12:00:05 +0800

    问答系统完成wcf服务端和python服务端的编写

---
+++ 问答系统/WCF服务端/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF服务端
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建一个基地址,MyService名称自己起
+            Uri uri1 = new Uri("http://47.94.102.147:8000/TTSService");
+            Uri uri2 = new Uri("http://47.94.102.147:8000/HandService");
+            Uri uri3 = new Uri("http://47.94.102.147:8000/QAService");
+
+
+
+            //创建一个Servicehost对象
+            ServiceHost host1 = new ServiceHost(typeof(TTSService), uri1);
+            ServiceHost host2 = new ServiceHost(typeof(HandService), uri2);
+            ServiceHost host3 = new ServiceHost(typeof(QAService), uri3);
+
+            WSHttpBinding bind1 = new WSHttpBinding();
+            bind1.Security.Mode = SecurityMode.None;
+
+            WSHttpBinding bind2 = new WSHttpBinding();
+            bind2.Security.Mode = SecurityMode.None;
+
+            WSHttpBinding bind3 = new WSHttpBinding();
+            bind3.Security.Mode = SecurityMode.None;
+
+
+            //为一个服务添加终结点,myHello名称自己起
+            host1.AddServiceEndpoint(typeof(ITTSContract), bind1, "TTSInterface");
+            host2.AddServiceEndpoint(typeof(IHandContract), bind2, "HandGestureInterface");
+            host3.AddServiceEndpoint(typeof(IQAContract), bind3, "QAInterface");
+            
+            //建立元数据行为对象
+            ServiceMetadataBehavior behavior1 = new ServiceMetadataBehavior();
+            //设置httpgetEnabled属性
+            behavior1.HttpGetEnabled = true;
+            //把这个服务元数据行为对象添加到宿主ServiceHost对象的行为集合中去
+            host1.Description.Behaviors.Add(behavior1);
+
+
+            //建立元数据行为对象
+            ServiceMetadataBehavior behavior2 = new ServiceMetadataBehavior();
+            //设置httpgetEnabled属性
+            behavior2.HttpGetEnabled = true;
+            //把这个服务元数据行为对象添加到宿主ServiceHost对象的行为集合中去
+            host2.Description.Behaviors.Add(behavior2);
+
+
+            //建立元数据行为对象
+            ServiceMetadataBehavior behavior3 = new ServiceMetadataBehavior();
+            //设置httpgetEnabled属性
+            behavior3.HttpGetEnabled = true;
+            //把这个服务元数据行为对象添加到宿主ServiceHost对象的行为集合中去
+            host3.Description.Behaviors.Add(behavior3);
+
+
+
+            //启动宿主，开启服务
+            host1.Open();
+            host2.Open();
+            host3.Open();
+
+
+            Console.WriteLine("host is ready");
+            Console.ReadLine();
+            //关闭服务
+            host1.Close();
+            host2.Close();
+            host3.Close();
+        }
+    }
+}
---
+++ 问答系统/问答系统(客户端)/Assets/Scripts/QAManager/QAManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ServiceModel;
+using UnityEngine;
+
+public class QAManager : MonoBehaviour {
+    EndpointAddress address;
+    WSHttpBinding binding;
+    QAServiceConstract.IQAContract channel;
+    ChannelFactory<QAServiceConstract.IQAContract> factory;
+    fvc.unet.start.SendQAMessage qaMessage = new fvc.unet.start.SendQAMessage(short.MaxValue - 4);
+	void Start () {
+        qaMessage.serverMsgDelegate = new fvc.unet.start.ServerMsgDelegate(ServerMsgReceive);
+        qaMessage.clientMsgDelegate = new fvc.unet.start.ClientMsgDelegate(ClientMsgRecevie);
+
+        address = new EndpointAddress("http://47.94.102.147:8000/QAService/QAInterface");
+        binding = new WSHttpBinding();
+        binding.Security.Mode = SecurityMode.None;
+        factory = new ChannelFactory<QAServiceConstract.IQAContract>(binding, address);
+        channel = factory.CreateChannel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+   /// <summary>
+    /// 服务端接收到消息回调的函数
+   /// </summary>
+   /// <param name="msg">接收传入的消息</param>
+   /// <returns>处理完的消息</returns>
+    private string ServerMsgReceive(string msg) {
+        return "";
+    }
+
+    /// <summary>
+    /// 客户端接收到消息回调的函数
+    /// </summary>
+    /// <param name="msg"></param>
+    private void ClientMsgRecevie(string msg) { 
+    
+    }
+
+    /// <summary>
+    /// 获取问题答案
+    /// </summary>
+    /// <param name="question">输入的问题</param>
+    /// <returns></returns>
+    private string GetAnswer(string question)
+    {
+        return channel.GetAnswer(question);
+    }
+}
